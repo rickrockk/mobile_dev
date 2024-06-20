e2fb1b87272b4d37490aec06d8ad541694055ee3
kirill <78549183+rickrockk@users.noreply.github.com>
2024-06-21 00:42:34 +0300

    added попытки сделать 8 лабу

--- views/Home/Home.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
-import Home from './Home';
-
-test('renders Home component correctly', () => {
-    const { getByText } = render(<Home />);
-    expect(getByText('Home')).toBeTruthy();
-});
--- views/Profile/Profile.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
-import Profile from "./Profile";
-
-test('renders Profile component correctly', () => {
-    const { getByText } = render(<Profile />);
-    expect(getByText('Profile')).toBeTruthy();
-});
